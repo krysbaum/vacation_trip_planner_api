a7dbaf4f415cea74e9cf976d90e85a3c566c11f1
Krys <krysbaum@gmail.com>
2024-04-28 08:31:08 -0700

    Add seed file for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(name: "Krys", email: "krys@example.com", password: "password", image_url: "https://avatars.githubusercontent.com/u/83841519?v=4")
+
+Trip.create!(user_id: 1, title: "PDX Staycation", image_url: "https://i.ebayimg.com/images/g/KIIAAOSwPVpct5w9/s-l1600.jpg", start_time: "2024-05-17", end_time: "2024-05-26")
+Trip.create!(user_id: 1, title: "Japan Trip", image_url: "https://img.freepik.com/premium-vector/japan-logo-vector-illustrations-suitable-your-logo_509354-327.jpg", start_time: "2025-02-01", end_time: "2024-03-15")
+
+Place.create!(trip_id: 1, address: "3765 N Mississippi Ave, Portland, OR", name: "Broder Nord", description: "This Scandanavian-style breakfast will be the perfect place to start our vacation.", image_url: "https://s3-media0.fl.yelpcdn.com/bphoto/ndfeqLRGNR48vZ7yQrKv-Q/348s.jpg", start_time: "2024-05-19", end_time: "2024-05-19")
